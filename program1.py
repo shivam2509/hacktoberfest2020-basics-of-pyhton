975a539d93382640abdca7f2328724ec6e19ccdf
Yogesh Singh <ysyogeshsingh1@gmail.com>
2020-10-25 14:33:43 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,13 +1,13 @@
 # This program adds two numbers
 
-num1 = int(input("Enter your first number "))
+number1 = int(input("Enter your first number "))
 
-num2 = int(input("Enter your second number "))
+number2 = int(input("Enter your second number "))
 
 # Add two numbers
-sum = num1 + num2
+sum = number1 + number2
 
 # Display the sum
 
-print("The sum of", num1, 'and', num2, 'is')
+print("The sum of", number1, 'and', number2, 'is')
 print(sum)
